7b0a55b8d5f702fab435c248d0b10ae2adbcd9c5
steckerhalter <steckerhalter@postmomentum.ch>
2015-03-04 09:26:30 +0100

    create dir

--- ci/run.sh
+++ ci/run.sh
@@ -9,15 +9,18 @@ trap "rm -rf ~/.emacs.d/" EXIT
 pwd=`pwd`
 
 if ! test -e /usr/local/bin/emacs; then
-    sudo apt-get install build-essential wget
-    sudo apt-get build-dep emacs23
-    wget http://ftp.gnu.org/gnu/emacs/emacs-24.4.tar.gz
-    tar -xzf emacs-24.4.tar.gz
-    cd emacs-24.4
-    ./configure &&\
-        make &&\
-        sudo make install
-    sudo apt-get install -qq git mercurial subversion bzr cvs
+    {
+        sudo apt-get install build-essential wget
+        sudo apt-get build-dep emacs23
+        wget http://ftp.gnu.org/gnu/emacs/emacs-24.4.tar.gz
+        tar -xzf emacs-24.4.tar.gz
+        cd emacs-24.4
+        ./configure &&\
+            make &&\
+            sudo make install
+        sudo apt-get install -qq git mercurial subversion bzr cvs
+    } > /dev/null
 fi
 
+mkdir ~/.emacs.d
 /usr/local/bin/emacs --batch --load $pwd/steckemacs.el
