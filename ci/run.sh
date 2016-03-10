7bf19a8522412247271364019bd7d8be88eebe5a
steckerhalter <steckerhalter@postmomentum.ch>
2016-03-10 21:54:47 +0100

    use emacs 24.5

--- ci/run.sh
+++ ci/run.sh
@@ -9,20 +9,16 @@ trap "rm -rf ~/.emacs.d/" EXIT
 pwd=`pwd`
 
 if ! test -e /usr/local/bin/emacs; then
-    {
-        >&2 echo "--- installing deps ---"
-        sudo apt-get update
-        sudo apt-get install build-essential wget
-        sudo apt-get build-dep emacs23
-        wget http://ftp.gnu.org/gnu/emacs/emacs-24.4.tar.gz
-        tar -xzf emacs-24.4.tar.gz
-        cd emacs-24.4
-        >&2 echo "--- building emacs ---"
-        ./configure &&\
-            make &&\
-            sudo make install
-        sudo apt-get install -qq git mercurial subversion bzr cvs
-    } > /dev/null
+    sudo apt-get update
+    sudo apt-get install build-essential wget
+    sudo apt-get build-dep emacs23
+    wget http://ftp.gnu.org/gnu/emacs/emacs-24.5.tar.gz
+    tar -xzf emacs-24.5.tar.gz
+    >&2 echo "--- building emacs ---"
+    ./configure &&\
+        make &&\
+        sudo make install
+    sudo apt-get install -qq git mercurial subversion bzr cvs
 fi
 
 echo "--- running tests ---"
