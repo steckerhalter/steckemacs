a775b49472603c725844f88e10bad8c74fa86fb3
steckerhalter <steckerhalter@postmomentum.ch>
2017-08-12 09:57:58 +0200

    remove org

--- steckemacs.el
+++ steckemacs.el
@@ -514,174 +514,6 @@ line instead."
             (lambda ()
               (turn-on-eldoc-mode))))
 
-;;;; org
-;; Outline-based notes management and organizer
-(use-package org
-  :quelpa (org :url "git://orgmode.org/org-mode.git" :fetcher git
-               :files ("lisp/*.el" "contrib/lisp/*.el" "doc/dir" "doc/*.texi"))
-  :bind
-  (("C-h T" . org-capture)
-   ("C-c i" . org-clock-in-last)
-   ("C-c o" . org-clock-out)
-   ("C-c C-9" . org-insert-subheading)
-   ("C-c C-0" . org-insert-todo-subheading)
-   ("C-h C-w" . org-cut-subtree))
-
-  :mode ("\\.org\\'" . org-mode)
-
-  :init
-  (setq org-startup-folded t)
-  (setq org-startup-indented nil)
-  (setq org-startup-with-inline-images t)
-  (setq org-startup-truncated t)
-  (setq org-src-fontify-natively t)
-  (setq org-src-tab-acts-natively t)
-  (setq org-edit-src-content-indentation 0)
-  (setq org-confirm-babel-evaluate nil)
-  (setq org-use-speed-commands t)
-  (setq org-refile-targets '((org-agenda-files :maxlevel . 3)))
-  (setq org-refile-use-outline-path 'file)
-  (setq org-html-postamble nil)
-  (setq org-agenda-dim-blocked-tasks t)
-  (setq org-habit-graph-column 60)
-  (setq org-enforce-todo-checkbox-dependencies t)
-  (setq org-enforce-todo-dependencies t)
-  (setq org-todo-keywords
-        '((sequence
-           "TODO(t)"
-           "WAITING(w)"
-           "SCHEDULED(s)"
-           "FUTURE(f)"
-           "|"
-           "DONE(d)")))
-  (setq org-todo-keyword-faces
-        '(("SCHEDULED" . warning)
-          ("WAITING" . font-lock-doc-face)
-          ("FUTURE" . "white")))
-  (setq org-log-into-drawer t) ;don't clutter files with state logs
-
-  :config
-  ;; remove C-' binding from org-mode (used to open shell)
-  (define-key org-mode-map (kbd "C-'") nil)
-  (require 'ox-org)
-  (require 'ox-md)
-  (add-to-list 'org-modules 'org-habit)
-  (add-to-list 'org-structure-template-alist
-               '("E" "#+BEGIN_SRC emacs-lisp\n?\n#+END_SRC\n"))
-  (add-to-list 'org-structure-template-alist
-               '("S" "#+BEGIN_SRC shell-script\n?\n#+END_SRC\n"))
-
-  ;; Don't use the same TODO state as the current heading for new heading
-  (defun my-org-insert-todo-heading ()
-    (interactive)
-    (org-insert-todo-heading t))
-  (define-key org-mode-map (kbd "<M-S-return>") 'my-org-insert-todo-heading)
-
-  ;; set parent todo autmatically to done
-  (defun org-summary-todo (n-done n-not-done)
-    "Switch entry to DONE when all subentries are done, to TODO otherwise."
-    (let (org-log-done org-log-states)   ; turn off logging
-      (org-todo (if (= n-not-done 0) "DONE" "TODO"))))
-  (add-hook 'org-after-todo-statistics-hook 'org-summary-todo)
-
-;;;;; org-list
-  (use-package org-list
-    ;; automatically set parent todos for checkboxes
-    ;; see http://thread.gmane.org/gmane.emacs.orgmode/42715
-    :init
-    (defun my-checkbox-list-complete ()
-      (save-excursion
-        (org-back-to-heading t)
-        (let ((beg (point)) end)
-          (end-of-line)
-          (setq end (point))
-          (goto-char beg)
-          (if (re-search-forward "\\[\\([0-9]*%\\)\\]\\|\\[\\([0-9]*\\)/\\([0-9]*\\)\\]" end t)
-              (if (match-end 1)
-                  (if (equal (match-string 1) "100%")
-                      ;; all done - do the state change
-                      (org-todo 'done)
-                    (org-todo 'todo))
-                (if (and (> (match-end 2) (match-beginning 2))
-                         (equal (match-string 2) (match-string 3)))
-                    (org-todo 'done)
-                  (org-todo 'todo)))))))
-    :config
-    (add-hook 'org-checkbox-statistics-hook (function my-checkbox-list-complete)))
-
-;;;;; org-agenda
-  (use-package org-agenda
-    :init
-    (defun my-org-agenda () (interactive) (org-agenda nil "n"))
-    (setq org-agenda-start-with-log-mode t)
-    (setq org-agenda-todo-ignore-scheduled 'future) ;don't show future scheduled
-    (setq org-agenda-todo-ignore-deadlines 'far)    ;show only near deadlines
-    (setq org-agenda-dim-blocked-tasks t)
-
-    :bind ("C-h C-n" . my-org-agenda)
-
-    :config
-    ;; add state to the sorting strategy of todo
-    (setcdr (assq 'todo org-agenda-sorting-strategy) '(todo-state-up priority-down category-keep))
-
-    ;; create the file for the agendas if it doesn't exist
-    (let ((agendas "~/.agenda_files"))
-      (unless (file-readable-p agendas)
-        (with-temp-file agendas nil))
-      (setq org-agenda-files agendas))
-
-    ;; display the agenda first
-    (setq org-agenda-custom-commands
-          '(("n" "Agenda and all TODO's"
-             ((alltodo "")
-              (agenda "")))))
-
-    ;; add new appointments when saving the org buffer, use 'refresh argument to do it properly
-    (defun my-org-agenda-to-appt-refresh () (org-agenda-to-appt 'refresh))
-    (defun my-org-mode-hook ()
-      (add-hook 'after-save-hook 'my-org-agenda-to-appt-refresh nil 'make-it-local))
-    (add-hook 'org-mode-hook 'my-org-mode-hook))
-
-  (use-package notifications
-    :config
-    (defun my-appt-disp-window-function (min-to-app new-time msg)
-      (notifications-notify :title (format "Appointment in %s min" min-to-app) :body msg))
-    (setq appt-disp-window-function 'my-appt-disp-window-function)
-    (setq appt-delete-window-function (lambda (&rest args))))
-
-;;;;; org-capture
-  (use-package org-capture
-    :init
-    (setq org-capture-templates
-          '(("t" "Task" entry (file "") "* TODO %?\n %a")
-            ("s" "Simple Task" entry (file "") "* TODO %?\n"))))
-
-;;;;; org-clock
-  (use-package org-clock
-    :init
-    (setq org-clock-idle-time 15)
-    (setq org-clock-in-resume t)
-    (setq org-clock-persist t)
-    (setq org-clock-persist-query-resume nil)
-    (setq org-clock-clocked-in-display 'both)
-    (setq org-clock-frame-title-format
-          (append '((t org-mode-line-string)) '(" ") frame-title-format))
-    (when (executable-find "xprintidle")
-      (setq org-x11idle-exists-p t)
-      (setq org-clock-x11idle-program-name "xprintidle"))
-
-    :config (org-clock-persistence-insinuate))
-
-;;;;; ox-latex
-  (use-package ox-latex
-    :config
-    (setq org-latex-listings 'minted)
-    (setq org-latex-pdf-process
-          '("pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"
-            "pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"
-            "pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"))
-    (add-to-list 'org-latex-packages-alist '("" "minted"))))
-
 ;;;; paren
 ;; highlight matching paren
 (use-package paren
@@ -884,10 +716,6 @@ line instead."
   (add-to-list 'company-backends 'company-files t)
   (add-to-list 'company-begin-commands 'outshine-self-insert-command)
   (setq company-backends (remove 'company-ropemacs company-backends))
-
-  (defun my-pcomplete-capf ()
-    (add-hook 'completion-at-point-functions 'pcomplete-completions-at-point nil t))
-  (add-hook 'org-mode-hook #'my-pcomplete-capf)
 
   (defun my-company-elisp-setup ()
     (set (make-local-variable 'company-backends)
@@ -1141,8 +969,6 @@ line instead."
   :config
   (require 'helm-config)
   (helm-mode 1)
-  (add-to-list 'helm-completing-read-handlers-alist '(org-refile)) ;helm-mode does not do org-refile well
-  (add-to-list 'helm-completing-read-handlers-alist '(org-agenda-refile)) ;same goes for org-agenda-refile
   (add-to-list 'helm-completing-read-handlers-alist '(dired-create-directory))
 
   (use-package helm-descbinds
@@ -1462,7 +1288,6 @@ Pass symbol-name to the function DOC-FUNCTION."
                   html-mode-hook
                   js-mode-hook
                   emacs-lisp-mode-hook
-                  org-mode-hook
                   text-mode-hook
                   ))
     (add-hook hook 'rainbow-mode)))
