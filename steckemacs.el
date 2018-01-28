6415cf7bd479410ec73c0d86444dc7e4bf843cb9
steckerhalter <steckerhalter@postmomentum.ch>
2018-01-28 17:10:43 +0100

    try build with auto download of emojis

--- steckemacs.el
+++ steckemacs.el
@@ -1021,6 +1021,7 @@ the user activate the completion manually."
 ;;;; emojify
 (use-package emojify
   :quelpa (emojify :fetcher github :repo "iqbalansari/emacs-emojify" :files (:defaults "data" "images"))
+  :custom (emojify-download-emojis-p t)
   :config (global-emojify-mode 1))
 
 ;;;; eval-sexp-fu
@@ -1796,7 +1797,7 @@ Pass symbol-name to the function DOC-FUNCTION."
 (use-package workgroups2
   :quelpa (workgroups2 :repo "pashinin/workgroups2" :fetcher github :files ("src/*.el"))
   :diminish (workgroups-mode)
-  :hook (window-setup . autostart)
+  ;:hook (window-setup . autostart)
   :init
   (defun autostart ()
     (erc-tls :server erc-server :port erc-port :nick erc-nick :full-name erc-user-full-name :password erc-password)
