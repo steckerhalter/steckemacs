5d63bea690a538b5e7de75d819c7e1b1af70e882
steckerhalter <steckerhalter@postmomentum.ch>
2018-01-28 19:58:55 +0100

    activate wip modes, show word-based diff for all hunks

--- steckemacs.el
+++ steckemacs.el
@@ -1404,11 +1404,13 @@ KEYS should be provided as with `kbd'."
   (setq git-commit-finish-query-functions nil)
   (setq magit-save-some-buffers nil) ;don't ask to save buffers
   (setq magit-set-upstream-on-push t) ;ask to set upstream
-  (setq magit-diff-refine-hunk t) ;show word-based diff for current hunk
+  (setq magit-diff-refine-hunk 'all) ;show word-based diff for all hunks
   (setq magit-default-tracking-name-function
         'magit-default-tracking-name-branch-only) ;don't track with origin-*
 
   :config
+  (setq magit-wip-after-save-mode 1)
+  (setq magit-wip-after-apply-mode 1)
   ;; Emacs Minor mode to automatically commit and push
   (use-package git-auto-commit-mode
     :quelpa (git-auto-commit-mode :fetcher github
